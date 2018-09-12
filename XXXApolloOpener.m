317f2824696a01f59b9f21025df2c19124b8d342
Vantablack <chongyaowei@gmail.com>
2018-09-12 16:39:31 +0800

    Add detection for the domain reddit.app.link and parse the query params
    - Get the original Reddit URL from $og_redirect
    - We can also get the Reddit comment/post from $deeplink_path or $android_deeplink_path

--- XXXApolloOpener.m
+++ XXXApolloOpener.m
@@ -23,16 +23,49 @@
         [url.host isEqualToString:@"reddit.com"] ||
         [url.host isEqualToString:@"m.reddit.com"] ||
         [url.host isEqualToString:@"old.reddit.com"] ||
+        [url.host isEqualToString:@"reddit.app.link"] ||
         [url.host containsString:@".reddit.com"]) {
-        
-        if ([url.path isEqualToString:@"/"] || [url.path isEqualToString:@""]) {
-            return [NSURL URLWithString:@"apollo://"];
-        } else {
-            return [NSURL URLWithString:[NSString stringWithFormat:@"apollo://reddit.com%@/?%@", url.path, url.query]];
+
+        if ([url.host isEqualToString:@"reddit.app.link"]) {
+            NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:url 
+                                                resolvingAgainstBaseURL:NO];
+            NSArray *queryItems = urlComponents.queryItems;
+            NSString *og_redirect = [self valueForKey:@"$og_redirect" 
+                            fromQueryItems:queryItems];
+
+            if ([og_redirect length] != 0) {
+                url = [NSURL URLWithString:og_redirect];
+            } else {
+                return nil;
+            }
         }
+
+        return [self createApolloURLFromURL:url];
     }
 
     return nil;
+}
+
+- (NSURL *)createApolloURLFromURL:(NSURL *)url {
+    if ([url.path isEqualToString:@"/"] || [url.path isEqualToString:@""]) {
+        return [NSURL URLWithString:@"apollo://"];
+    } else {
+        return [NSURL URLWithString:[NSString stringWithFormat:@"apollo://reddit.com%@/?%@", url.path, url.query]];
+    }
+}
+
+/*
+Thanks to:
+- https://stackoverflow.com/questions/8756683/best-way-to-parse-url-string-to-get-values-for-keys
+- https://stackoverflow.com/questions/8756683/best-way-to-parse-url-string-to-get-values-for-keys/26406426#26406426
+*/
+- (NSString *)valueForKey:(NSString *)key fromQueryItems:(NSArray *)queryItems {
+
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name=%@", key];
+    NSURLQueryItem *queryItem = [[queryItems 
+                                  filteredArrayUsingPredicate:predicate]
+                                 firstObject];
+    return queryItem.value;
 }
 
 @end
